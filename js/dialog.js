49a2ef7ee40c41e757be4ab6dbdd4a03a0b13f5d
vzuev-ha <65515070+vzuev-ha@users.noreply.github.com>
2020-06-22 01:17:25 +0300

    13. Учебный проект: в движении

---
+++ js/dialog.js
@@ -0,0 +1,105 @@
+'use strict';
+
+(function () {
+
+  function onClickWizardAndFireball(evt) {
+    var setupWizard = document.querySelector('.setup-player');
+    var newColor;
+
+    if (evt.target) {
+      if (evt.target.classList.contains('wizard-coat')) {
+        newColor = window.utils.getRandomCoatColor();
+        setupWizard.querySelector('input[name="coat-color"]').value = newColor;
+        evt.target.style.fill = newColor;
+      } else if (evt.target.classList.contains('wizard-eyes')) {
+        newColor = window.utils.getRandomEyesColor();
+        setupWizard.querySelector('input[name="eyes-color"]').value = newColor;
+        evt.target.style.fill = newColor;
+      } else if (evt.target.classList.contains('setup-fireball')) {
+        newColor = window.utils.getRandomFireballColor();
+        setupWizard.querySelector('input[name="fireball-color"]').value = newColor;
+        evt.target.style.backgroundColor = newColor;
+      }
+    }
+  }
+
+
+  function onPopupEscPress(evt) {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      if (evt.target.name !== 'username') {
+        closePopup();
+      }
+    }
+  }
+
+  function closePopup() {
+    // Найдем окно настроек и скроем его
+    var setupDialog = window.setup.setupDialogElement;
+    setupDialog.classList.add('hidden');
+
+    document.removeEventListener('keydown', onPopupEscPress);
+
+    // Удалим  обработчик клика по персонажу и смены цветов
+    var setupPlayer = document.querySelector('.setup-player');
+    setupPlayer.removeEventListener('click', onClickWizardAndFireball);
+
+    // Удалим обработчик нажатия мыши на кнопку выбора файла .upload
+    window.setup.setupDialogHandle.removeEventListener('mousedown', window.mover.onSetupDialogMouseDown);
+
+    // Восстановим положение окна настроек
+    setupDialog.style.left = window.setup.setupDialogDefaultCoords.x + 'px';
+    setupDialog.style.top = window.setup.setupDialogDefaultCoords.y + 'px';
+  }
+
+  function openPopup() {
+    // Покажем окно настроек
+    var setupDialog = window.setup.setupDialogElement;
+    setupDialog.classList.remove('hidden');
+
+    // Найдем блок для показа магов
+    var wizardsPlaceholder = document.querySelector('.setup-similar-list');
+
+    // Заполним этот блок Фрагментом
+    window.setup.fillWizardsBlock(wizardsPlaceholder);
+
+    // Отобразим блок со списком магов
+    document.querySelector('.setup-similar').classList.remove('hidden');
+
+    // Привяжем к документу обработчик закрытия по Esc
+    document.addEventListener('keydown', onPopupEscPress);
+
+
+    // Привяжем обработчики закрытия к крестику по клику и Enter
+    var setupClose = window.setup.setupDialogElement.querySelector('.setup-close');
+
+    setupClose.addEventListener('click', function () {
+      closePopup();
+    });
+
+    setupClose.addEventListener('keydown', function (evt) {
+      if (evt.key === 'Enter') {
+        closePopup();
+      }
+    });
+
+    // Привяжем через делегирование обработчик клика по персонажу и смены цветов
+    var setupPlayer = document.querySelector('.setup-player');
+    setupPlayer.addEventListener('click', onClickWizardAndFireball);
+
+
+    // Навесим обработчик нажатия мыши на кнопку выбора файла .upload
+    window.setup.setupDialogHandle.addEventListener('mousedown', window.mover.onSetupDialogMouseDown);
+
+    // Сохраним положение окна настроек
+    window.setup.setupDialogDefaultCoords.x = setupDialog.offsetLeft;
+    window.setup.setupDialogDefaultCoords.y = setupDialog.offsetTop;
+  }
+
+
+  // Чтобы не загромождать секцию экспорта, опишем функцию выше, а здесь сошлемся на нее
+  window.dialog = {
+    openPopup: openPopup
+  };
+
+})();
---
+++ js/utils.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function () {
+  // Объявим константы
+  var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+
+  var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+
+  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+  /**
+   * Получение случайного целого числа в заданном интервале, включительно
+   * @param {int} min
+   * @param {int} max
+   * @return {int} Случайное число
+   */
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
+  }
+
+  /*
+    Другой вариант получения рандомного числа от 0 до max
+    return COLORS[Math.floor(COLORS.length * Math.random())];
+   */
+
+  window.utils = {
+    getRandomName: function () {
+      var randomNameId = getRandomIntInclusive(0, FIRST_NAMES.length - 1);
+      return FIRST_NAMES[randomNameId] + ' ' + LAST_NAMES[randomNameId];
+    },
+
+    getRandomCoatColor: function () {
+      return COAT_COLORS[getRandomIntInclusive(0, COAT_COLORS.length - 1)];
+    },
+
+    getRandomEyesColor: function () {
+      return EYES_COLORS[getRandomIntInclusive(0, EYES_COLORS.length - 1)];
+    },
+
+    getRandomFireballColor: function () {
+      return FIREBALL_COLORS[getRandomIntInclusive(0, FIREBALL_COLORS.length - 1)];
+    }
+  };
+
+})();
